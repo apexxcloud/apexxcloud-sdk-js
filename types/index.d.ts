8608ab9ec79fac7107918dceae2a85a7fe11b0d8
jagan-blip <jaganscientistcr6@gmail.com>
2024-12-24 17:51:49 +0530

    fix: types fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-declare module 'apexxcloud-sdk-js' {
+declare module '@apexxcloud/sdk-js' {
   interface UploadCallbacks {
     onProgress?: (event: {
       loaded: number;
@@ -29,55 +29,15 @@ declare module 'apexxcloud-sdk-js' {
     }) => void;
   }
 
-  interface MultipartUploadCallbacks extends UploadCallbacks {
-    onPartStart?: (event: {
-      type: string;
-      timestamp: Date;
-      partNumber: number;
-      totalParts: number;
-      chunkSize: number;
-    }) => void;
-    onPartProgress?: (event: {
-      type: string;
-      timestamp: Date;
-      partNumber: number;
-      totalParts: number;
-      loaded: number;
-      total: number;
-      progress: number;
-    }) => void;
-    onPartComplete?: (event: {
-      type: string;
-      timestamp: Date;
-      partNumber: number;
-      totalParts: number;
-      etag: string;
-    }) => void;
-    onPartError?: (event: {
-      type: string;
-      timestamp: Date;
-      partNumber: number;
-      totalParts: number;
-      error: Error;
-    }) => void;
-  }
-
-  class StorageSDK {
+ 
+  export  default class StorageSDK {
     constructor(config?: Record<string, any>);
     
     files: {
       upload(signedUrl: string, file: File, callbacks?: UploadCallbacks): Promise<any>;
-      uploadMultipart(
-        file: File,
-        config: {
-          startUrl: string;
-          uploadUrls: string[];
-          completeUrl: string;
-          partSize?: number;
-        } & MultipartUploadCallbacks
-      ): Promise<any>;
+      
     };
   }
 
-  export = StorageSDK;
+
 } 
